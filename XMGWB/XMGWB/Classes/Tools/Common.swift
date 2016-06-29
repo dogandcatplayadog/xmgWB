d2c83c87ab8cd64962b70abb12a5756b9869a2ab
dogandcatplayadog <13631611448@163.com>
2016-06-29 21:33:09 +0800

    haha

---
+++ XMGWB/XMGWB/Classes/Tools/Common.swift
@@ -0,0 +1,29 @@
+//
+//  Common.swift
+//  XMGWB
+//
+//  Created by xiaomage on 15/11/9.
+//  Copyright © 2015年 xiaomage. All rights reserved.
+//
+
+import Foundation
+
+// MARK: - 保存全局通知
+/// 自定义转场显示通知
+let XMGPopoverViewControllerShowClick = "XMGPopoverViewControllerShowClick"
+
+/// 自定义转场消失通知
+let XMGPopoverViewControllerDismissClick = "XMGPopoverViewControllerDismissClick"
+
+/// 切换更控制器通知
+let XMGRootViewControllerChnage = "XMGRootViewControllerChnage"
+
+
+/// 显示图片浏览器通知
+let XMGPhotoBrowserShow = "XMGPhotoBrowserShow"
+
+// MARK: - 授权信息
+let WB_App_Key = "2680353984"//"4102358945"
+let WB_App_Secret = "7f735fdcccbae8d8b44630f135888967"//"63cb5e14cf56f8c14f090ea7746f2482"
+let WB_Redirect_URI = "http://www.520it.com"
+
